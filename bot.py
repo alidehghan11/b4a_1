68f652cc8b530057b8b67fc2aa1b912c32de57c0
alidehghan11 <77375038+alidehghan11@users.noreply.github.com>
2023-04-30 08:24:46 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,12 @@
-import time
+from datetime import datetime
 import requests
 
 Token_tele = "5264261012:AAHlJvK80L2a8n3F-irke03vj1zqB757n-Q"
 ID_tele = "1501835350"
-post= 
+
+now = datetime.now() # current date and time
+post= now.strftime("%m/%d/%Y, %H:%M:%S")
+
 url = 'https://api.telegram.org/bot' + Token_tele + '/sendMessage?text=' + post + '&chat_id=' + ID_tele 
 requests.get(url)
 
